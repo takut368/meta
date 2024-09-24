7ea32f7a467a6567d3469ec12e6451e605cb63c5
takut368 <44548546+takut368@users.noreply.github.com>
2024-09-24 22:40:27 +0900

    Add files via upload

---
+++ threads/sample/threads_api-main/const.php
@@ -0,0 +1,15 @@
+<?php
+
+// アプリ設定
+define('APPID', ''); // ThreadsアプリID (「アプリの設定」>「ベーシック」)
+define('APISECRET', ''); // Threads App Secret(「アプリの設定」>「ベーシック」
+define('REDIRECT_URI', ''); // （ユースケース > アプリ選択 > カスタマイズ > 設定）
+
+// クラス設定
+define('BUFFER_DAYS', 7); // トークンの有効期限を前もって更新する日数
+
+// データベースの設定
+define('DBNAME', 'kp');
+define('HOST', 'db');
+define('DBUSER', 'root');
+define('DBPASSWORD', 'password');
